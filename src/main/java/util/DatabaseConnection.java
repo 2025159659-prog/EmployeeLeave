7d98a4ab698cb495d7cf473771a0508ba61fd5f2
WAN MUHAMMAD A'BID NADZRI BIN WAN MOHD ZAWAWI <2025159659@student.uitm.edu.my>
2026-01-18 10:39:02 +0800

    Update DatabaseConnection.java

--- src/main/java/util/DatabaseConnection.java
+++ src/main/java/util/DatabaseConnection.java
@@ -1,5 +1,3 @@
-package util;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.net.URI;
@@ -9,22 +7,21 @@ public class DatabaseConnection {
     public static Connection getConnection() throws Exception {
 
         String dbUrl = System.getenv("DATABASE_URL");
-
         if (dbUrl == null) {
-            throw new RuntimeException("DATABASE_URL not found");
+            throw new RuntimeException("DATABASE_URL is not set");
         }
 
         URI uri = new URI(dbUrl);
 
-        String userInfo = uri.getUserInfo();
-        String username = userInfo.split(":")[0];
-        String password = userInfo.split(":")[1];
+        String[] userInfo = uri.getUserInfo().split(":");
+        String username = userInfo[0];
+        String password = userInfo[1];
 
-        String jdbcUrl = "jdbc:postgresql://" 
-                + uri.getHost() 
-                + ":" + uri.getPort() 
-                + uri.getPath()
-                + "?sslmode=require";
+        String jdbcUrl = "jdbc:postgresql://" +
+                uri.getHost() + ":" +
+                uri.getPort() +
+                uri.getPath() +
+                "?sslmode=require";
 
         Class.forName("org.postgresql.Driver");
         return DriverManager.getConnection(jdbcUrl, username, password);
